81af74f38868a6309b44aa06f27bd364832c4b35
NicholasETerry <nicholas.e.terry@gmail.com>
2021-04-19 22:50:12 -0500

    added custom objects in practice section

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,27 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const guitar = {
+    name : "Taylor 414ce",
+    acoustic : true,
+    electric : true,
+    isActive : false,
+    color : "natural",
+    strings : 6,
+    switchToElectric : function(switchState){
+        this.isActive = true;
+    },
+
+}
+const guitarTwo = {
+    name : "Taylor 814ce",
+    acoustic : true,
+    electric : true,
+    isActive : false,
+    color : "natural",
+    strings : 12,
+    switchToElectric : function(switchState){
+        this.isActive = true;
+    },
+}
